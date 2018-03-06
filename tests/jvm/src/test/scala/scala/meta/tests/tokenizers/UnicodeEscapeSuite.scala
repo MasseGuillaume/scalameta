a33b5dd6a2e3c1b43d9bc990ca3df9f62395662e
Guillaume Massé <masgui@gmail.com>
2018-03-06 15:33:09 +0100

    .

--- tests/jvm/src/test/scala/scala/meta/tests/tokenizers/UnicodeEscapeSuite.scala
+++ tests/jvm/src/test/scala/scala/meta/tests/tokenizers/UnicodeEscapeSuite.scala
@@ -6,14 +6,19 @@ import org.langmeta.internal.io.InputStreamIO
 import org.scalameta.logger
 
 class UnicodeEscapeSuite extends BaseTokenizerSuite with DiffAssertions {
+
+  def fromResource(path: String): String = 
+    new String(
+      InputStreamIO.readBytes(this.getClass.getClassLoader.getResourceAsStream(path)),
+      StandardCharsets.UTF_8
+    )
+
   // Read tests from external file because scalac processes string literals in source
   // @ """ s"${x}\uef17" """.length
   // res0: Int = 10
   // by reading from external file escapes like `\uef17` are represented
   // as 6 characters instead of one.
-  val tests = new String(
-    InputStreamIO.readBytes(this.getClass.getClassLoader.getResourceAsStream("unicode.txt")),
-    StandardCharsets.UTF_8)
+  val tests = fromResource("unicode.txt")
 
   // asserts that tokenize(code).syntax == code
   def checkRoundtrip(original: String): Unit = {
@@ -27,5 +32,8 @@ class UnicodeEscapeSuite extends BaseTokenizerSuite with DiffAssertions {
   tests.lines.foreach { line =>
     checkRoundtrip(line)
   }
+
+  val test2 = fromResource("unicode2.txt")  
+  checkRoundtrip(test2)
 
 }
