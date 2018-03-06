d8686a6f6448c6ebe7b1457ddf4dedb6cc4e3ecc
Guillaume Massé <masgui@gmail.com>
2018-03-06 15:33:09 +0100

    Failing test case for #1384

--- tests/jvm/src/test/scala/scala/meta/tests/tokenizers/UnicodeEscapeSuite.scala
+++ tests/jvm/src/test/scala/scala/meta/tests/tokenizers/UnicodeEscapeSuite.scala
@@ -1,26 +1,44 @@
 package scala.meta.tests.tokenizers
 
+import scala.meta._
+
 import java.nio.charset.StandardCharsets
 import scala.meta.testkit.DiffAssertions
 import org.langmeta.internal.io.InputStreamIO
 import org.scalameta.logger
 
 class UnicodeEscapeSuite extends BaseTokenizerSuite with DiffAssertions {
+
+  def fromResource(path: String): String = 
+    new String(
+      InputStreamIO.readBytes(this.getClass.getClassLoader.getResourceAsStream(path)),
+      StandardCharsets.UTF_8
+    )
+
   // Read tests from external file because scalac processes string literals in source
   // @ """ s"${x}\uef17" """.length
   // res0: Int = 10
   // by reading from external file escapes like `\uef17` are represented
   // as 6 characters instead of one.
-  val tests = new String(
-    InputStreamIO.readBytes(this.getClass.getClassLoader.getResourceAsStream("unicode.txt")),
-    StandardCharsets.UTF_8)
+  val tests = fromResource("unicode.txt")
 
   // asserts that tokenize(code).syntax == code
   def checkRoundtrip(original: String): Unit = {
-    test(logger.revealWhitespace(original)) {
+    test("roundtrip: " + logger.revealWhitespace(original)) {
       val tokens = tokenize(original)
       val obtained = tokens.mkString
       assertNoDiff(obtained, original)
+    }
+  }
+
+  def checkOneWay(original: String): Unit = {
+    test("one way " + logger.revealWhitespace(original)) {
+      val obtained = 
+        original.parse[Term].get match {
+          case Lit.String(value) => value
+        }
+
+      assert(obtained == original)
     }
   }
 
@@ -28,4 +46,12 @@ class UnicodeEscapeSuite extends BaseTokenizerSuite with DiffAssertions {
     checkRoundtrip(line)
   }
 
+  val tests2 = fromResource("unicode2.txt")
+  tests2.lines.foreach { line =>
+    checkRoundtrip(line)
+  }
+
+  tests2.lines.foreach { line =>
+    checkOneWay(line)
+  }
 }
